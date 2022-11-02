ca456d32c4252b88aec8a08ad6c99a2e7832bbeb
taoka-toshiaki <71550806+taoka-toshiaki@users.noreply.github.com>
2022-11-02 16:45:04 +0900

    常時ディレクトリ監視。任意の画像ファイルだけを別のディレクトリへ移動するコード。

---
+++ File_Check.php
@@ -0,0 +1,20 @@
+<?php
+while(true){
+    if($result = is_scandir("./upload")){
+        foreach($result as $key=>$value){
+            rename("./upload/$value","./data/$value");
+        }
+    }
+}
+
+function is_scandir(string $dirname="",array $ext_list = ["png","jpg"]){
+    $is_filelest = [];
+    $result = scandir($dirname);
+    foreach($result as $key=>$value){
+        $ext = substr($value, strrpos($value, '.') + 1);
+        if(in_array($ext,$ext_list,false)!== false){
+            $is_filelest[] = $value;
+        }
+    }
+    return count($is_filelest)>0 ? $is_filelest : false;
+}
